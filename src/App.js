79c535765822c678a16074ff8443e56ab98d6047
Jau <jaumecann@gmail.com>
2021-06-03 08:01:14 +0200

    Commit inicial, primers components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Userbox from './components/Userbox';
+import Card from './UI/Card'
+
+
+function App() {
+  return (
+    <div>
+      <Card>
+
+      <Userbox />
+     
+     </Card>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Userbox.js
@@ -0,0 +1,12 @@
+
+
+const Userbox = () => {
+
+    return (
+        <div>
+            <h1>Holificio Holifai</h1>
+        </div>
+    )
+}
+
+export default Userbox
